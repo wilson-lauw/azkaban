80bff3bc8c036b5f73a8871bc45384ed0b7153b5
traveloka <wilson.lauw@traveloka.com>
2018-03-09 14:57:58 +0800

    add reload when starting web server

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 /scripts/wait_for_port_ready.py 3306 5 3
-/azkaban-web-server/bin/start-web.sh 
+/azkaban-web-server/bin/start-web.sh
+/scripts/reload_exec.py
 tail -f /dev/null
