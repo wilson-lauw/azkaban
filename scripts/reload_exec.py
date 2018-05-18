c3d29e98d040a13244365122c2acb1a3d7cf232c
Wilson Lauw <wilson.lauw@traveloka.com>
2018-05-18 14:49:19 +0800

    3.47.0-kubernetes (#21)
    *  kubernetes deployment (#11)
    
    * kubernetes deployment
    
    * add reload when starting web server
    
    * add waiting for port to be open
    
    * make port waiting interval 1 sec
    
    * add logging
    
    * add local reload
    
    * move sync to web server pod (#6)
    
    * move sync to web pod
    
    * flush stdout
    
    * move sleep
    
    * fix project id
    
    * Update README.md
    
    * Update README.md
    
    * Update azkaban.properties
    
    * make requests more robust
    
    * add entry in gitignore
    
    * added sleep to allow notification for failed jobs
    
    added sleep to allow notification for failed jobs when scaling down
    
    * re-execute flows in downscale (#9)
    
    * 1 day grace period
    
    * 3 hours grace period
    
    * grab grace period from yaml file
    
    * small typo fix
    
    * Update README.md
    
    * add documentation
    
    * Update azkaban.properties
    
    * change GC options
    
    * Update exec-hpa.yaml
    
    * change service type
    
    * fix history filter
    
    * Update azkaban.properties
    
    * Update azkaban.properties
    
    * Update azkaban.properties
    
    * always reload exec
    
    * add logging
    
    *  fix mem info fetching (#17)
    
    * add resource request
    
    * change mem req
    
    * fix mem info fetching
    
    * fix test case
    
    * Update azkaban.properties
    
    * Update README.md
    
    * Update exec-hpa.yaml
    
    * Update Dockerfile-sync
    
    * increase hpa limit
    
    * exclude evicted pods from count
    
    * clean evicted pods
    
    * Update sync_exec.py
    
    * remove memory from hpa
    
    * Update exec-hpa.yaml
    
    * add pdb YAML
    
    * change GCP region
    
    * change db name
    
    * tidy up dockerfile
    
    * add more pdb
    
    * add more pdb
    
    * Update sync_exec.py
    
    * Update README.md
    
    * handle exec assign failure
    
    * stabilize azkaban sync

---
+++ scripts/reload_exec.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import requests
+import sys
+import time
+from wait_for_port_ready import wait_for_port_ready
+import traceback
+
+def reload_exec(local=False):
+
+    retries = 0
+    retry_count = 15
+    success = False
+    while not success:
+        try:
+            url = 'http://web.default.svc.cluster.local'
+            if local:
+                url = 'http://localhost:8081'
+                wait_for_port_ready(8081, 15)
+            payload = {'action': 'login', 'username': 'admin', 'password': 'admin'}
+
+            r = requests.post(url, data=payload, timeout=5)
+            print r.status_code
+            print r.text
+
+            session_id = r.json()['session.id']
+            payload = {'session.id': session_id, 'ajax': 'reloadExecutors'}
+
+            r = requests.post(url + '/executor', data=payload, timeout=5)
+            print r.status_code
+            print r.text
+
+            if r.json()['status'] == 'success':
+                success = True
+
+            if not success:
+                raise Exception('Reload executors failed')
+        
+        except Exception as ex:
+            print(traceback.format_exc())
+            sys.stdout.flush()
+            
+            retries += 1
+            if retries > retry_count:
+                return success
+            print 'waiting for 1 seconds...'
+            time.sleep(1)
+
+    return success
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        reload_exec(local=True)
+    else:
+        reload_exec()
