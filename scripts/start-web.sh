a21ee9fd24580a6a5b926fc6ee58d55bc663334b
traveloka <wilson.lauw@traveloka.com>
2018-05-22 16:44:14 +0800

    wait longer for port 3306

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/scripts/wait_for_port_ready.py 3306 15
+/scripts/wait_for_port_ready.py 3306 30
 /azkaban-web-server/bin/start-web.sh
 /scripts/wait_for_port_ready.py 8081 45
 /scripts/reload_exec.py local
