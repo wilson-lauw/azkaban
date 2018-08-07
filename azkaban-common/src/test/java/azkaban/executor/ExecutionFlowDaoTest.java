ef4c70663a181ce8e8ab80762ba04ccd7396bbc3
traveloka <wilson.lauw@traveloka.com>
2018-08-07 23:43:53 +0800

    fix

--- azkaban-common/src/test/java/azkaban/executor/ExecutionFlowDaoTest.java
+++ azkaban-common/src/test/java/azkaban/executor/ExecutionFlowDaoTest.java
@@ -39,7 +39,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ExecutionFlowDaoTest {
