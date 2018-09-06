5e08d9e9605bd88de0b5ce4aac4f03a46c4d7ca8
traveloka <wilson.lauw@traveloka.com>
2018-09-06 19:25:43 +0800

    put config file to kube secrets

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -3,7 +3,7 @@
 cp /secrets/azkaban-properties/azkaban.properties /azkaban/conf
 cp /secrets/azkaban-properties/azkaban-users.xml /azkaban/conf
 /scripts/wait_for_port_ready.py 3306 30
-/azkaban-web-server/bin/start-web.sh
+/azkaban/bin/start-web.sh
 /scripts/wait_for_port_ready.py 8081 45
 /scripts/reload_exec.py local
 tail -f /dev/null
