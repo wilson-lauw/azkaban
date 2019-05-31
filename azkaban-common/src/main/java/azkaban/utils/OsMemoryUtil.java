f61c80f6bedb9fd5de485f6cc112b8d5cedd909d
traveloka <wilson.lauw@traveloka.com>
2019-05-31 13:53:32 +0800

    revert to merge

--- azkaban-common/src/main/java/azkaban/utils/OsMemoryUtil.java
+++ azkaban-common/src/main/java/azkaban/utils/OsMemoryUtil.java
@@ -23,9 +23,7 @@ class OsMemoryUtil {
   private static final String MEM_INFO_FILE = "/proc/meminfo";
 
   private static final ImmutableSet<String> MEM_KEYS = ImmutableSet
-      .of("MemAvailable");
-      //.of("MemFree", "Buffers", "Cached", "SwapFree");
-
+      .of("MemFree", "Buffers", "Cached", "SwapFree");
 
   /**
    * Includes OS cache and free swap.
