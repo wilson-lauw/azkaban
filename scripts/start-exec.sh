044740dd5204c0ced58701d567245051ef13b1da
traveloka <wilson.lauw@traveloka.com>
2019-07-01 23:45:43 +0800

    commit

--- scripts/start-exec.sh
+++ scripts/start-exec.sh
@@ -7,4 +7,4 @@ cp /secrets/azkaban-users-xml/azkaban-users.xml /azkaban/conf
 /scripts/wait_for_port_ready.py 12321 15
 /scripts/executor_action.py activate
 /scripts/reload_exec.py
-tail -f /dev/null
+tail -f /azkaban/executorServerLog_*
