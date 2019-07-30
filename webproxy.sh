ecb3e1d27c7c351b620b09d6bb77f030423bc9fb
Wilson <wilson@ruangguru.com>
2019-07-30 14:54:31 +0800

    commit

--- webproxy.sh
+++ webproxy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-
-pod_name=$(kubectl get po|grep web|grep 3/3|grep Running|awk '{print $1}')
-kubectl port-forward $pod_name 8081:8081
+set -e
+namespace="${1}"
+pod_name=$(kubectl -n $namespace get po|grep web|grep 3/3|grep Running|awk '{print $1}')
+kubectl -n $namespace port-forward $pod_name 8081:8081
